ae572a7417273ff68ceb7491b46ad5efcda49c70
Rus P <texturered@gmail.com>
2025-03-26 15:52:17 +0100

    init vite build, add vscode json files

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/extension.ts',
+      formats: ['cjs'],
+    },
+    rollupOptions: {
+      external: ['vscode', 'node-fetch', 'util', 'path', 'fs'],
+    },
+    outDir: 'dist',
+    sourcemap: true,
+    minify: false,
+  },
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
+});
