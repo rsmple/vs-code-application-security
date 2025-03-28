d6b78ddbc9c42b2ff617841fb67ca73f46c9b13a
Rus P <texturered@gmail.com>
2025-03-28 17:07:47 +0100

    EXT: add eslint plugin

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import plugin from 'eco-vue-js/eslint/plugin'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,js,mts,tsx,vue,json}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**'],
+  },
+
+  ...plugin.configs.recommended,
+]
